17cba78bd07b389c3af87134a0a5f070232dc5ac
Dickson Ma <dickson_ma@jstar.email>
2024-02-19 18:44:53 +0800

    apply no cache to get gamelist

--- app/[locale]/utils/getGamelist.js
+++ app/[locale]/utils/getGamelist.js
@@ -1,7 +1,8 @@
 export default async function GetGamelist(language) {
   try {
     const res = await fetch(
-      `https://smakermicsvc.back138.com/api/opgateway/v1/op/demo/GameList?opId=kkgaming&lang=${language}`
+      `https://smakermicsvc.back138.com/api/opgateway/v1/op/demo/GameList?opId=kkgaming&lang=${language}`,
+      { cache: "no-store" }
     );
     const games = await res.json();
     return games;
